445e38c500d2ba759abbe4ff54de2a353711dadc
amiedawn <amie.d.carroll@gmail.com>
2021-03-17 19:34:01 -0600

    working on sendgrid

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import { validateEmail } from "../utils/helpers";
+const sgMail = require("@sendgrid/mail");
 
 function Contact() {
   const [formState, setFormState] = useState({
@@ -36,6 +37,39 @@ function Contact() {
       }
     }
   };
+
+  // send email via twilio sendgrid
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: "fstacoshop@gmail.com",
+    from: "fstacoshop@gmail.com", // Use the email address or domain you verified above
+    subject: "FS Taco Shop contact form data",
+    text: "and easy to do anywhere, even with Node.js",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  };
+  //ES6
+  sgMail.send(msg).then(
+    () => {},
+    (error) => {
+      console.error(error);
+
+      if (error.response) {
+        console.error(error.response.body);
+      }
+    }
+  );
+  //ES8
+  (async () => {
+    try {
+      await sgMail.send(msg);
+    } catch (error) {
+      console.error(error);
+
+      if (error.response) {
+        console.error(error.response.body);
+      }
+    }
+  })();
 
   return (
     <section>
