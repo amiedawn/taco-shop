9be77bd764bac7c3ef7e58ba59b9980639a6a5ec
Kelsey Freeborg <kelsey.freeborg@gmail.com>
2021-03-18 20:07:27 -0700

    multiple to cart working

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -43,7 +43,7 @@ export const reducer = (state, action) => {
     case ADD_MULTIPLE_TO_CART:
       return {
         ...state,
-        cart: [...state.cart, ...action.items],
+        cart: [...state.items, ...action.items],
       };
 
     case REMOVE_FROM_CART:
