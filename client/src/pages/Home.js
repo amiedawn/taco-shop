ce3852a3514c09406b1cdf203fd7ac2c6ab83bf0
kristinmoon <71245424+kristinmoon@users.noreply.github.com>
2021-03-20 00:17:20 -0700

    config materialize

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,8 +4,26 @@ import React from 'react';
 import Cart from '../components/Cart';
 
 const Home = () => (
-  <div className="container">
-    <p>Super Cool Front page INCOMING!!!</p>
+  <div id="index-banner" className="parallax-container">
+    <div className="section no-pad-bot">
+      <div className="container">
+        <h1 className="header center teal-text text-lighten-2">Parallax Template</h1>
+        <div className="row center">
+          <h5 className="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+        </div>
+        <div className="row center">
+          {/* <a
+          href="http://materializecss.com/getting-started.html" 
+          id="download-button" 
+          className="btn-large waves-effect waves-light teal lighten-1">
+            Get Started
+          </a> */}
+        </div>
+      </div>
+    </div>
+    <div className="parallax">
+      <img src="ingredients.jpeg" alt="taco ingredients" />
+    </div>
     <Cart />
   </div>
 );
